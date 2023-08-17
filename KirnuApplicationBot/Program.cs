b8d2b4946635ffa9c7730da4c54e07a24c777b14
Markus Peräkylä <markus.perakyla@gmail.com>
2023-08-17 22:18:11 +0300

    initial commit

---
+++ KirnuApplicationBot/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace KirnuApplicationBot
+{
+    class Program
+    {
+        public static Task Main(string[] args) => new Program().MainAsync();
+
+        public async Task MainAsync()
+        {
+            
+        }
+    }
+}
